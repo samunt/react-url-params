f8371f94237551f0056ee5351910f4747cda5d04
samunt <sammy.unterman@gmail.com>
2020-06-23 09:04:18 -0400

    multiple params plus explanantion of render vs component

--- router-url-params/src/App.js
+++ router-url-params/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
 import Food from './Food'
+import Meal from './Meal'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* Using render allows you to pass in multiple props */}
       <Route exact
              path="/food/:name"
              render={(routeParams) => <Food authenticated={true} {...routeParams}/>}/>
+      {/* Using component automatically passes in routeParams, but doesn't let you pass in anything else */}
+      <Route exact
+             path="/food/:foodName/drink/:drinkName"
+             component={Meal}/>
+
     </div>
   );
 }
---
+++ router-url-params/src/Meal.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Meal extends Component {
+    render() {
+        const foodName = this.props.match.params.foodName;
+        const drinkName = this.props.match.params.drinkName;
+        const foodUrl = `https://source.unsplash.com/1600x900/?${foodName}`;
+        const drinkUrl = `https://source.unsplash.com/1600x900/?${drinkName}`;
+
+        return (
+            <div>
+                <h1>This is a meal of {foodName} and {drinkName}</h1>
+                <img src={foodUrl} alt={foodName}/>
+                <img src={drinkUrl} alt={drinkName}/>
+            </div>
+        )
+    }
+}
+export default Meal
